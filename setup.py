16022c669550197ef3bb28bf00b3a52c6a55e045
roshan482 <thokalroshan2@gmail.com>
2025-10-08 22:18:56 +0530

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,11 @@
+
+from setuptools import find_packages, setup
+
+setup(
+    name="personal-assistant",
+    version="0.1.0",
+    author="Roshan Thokal",
+    author_email="thokalroshan2@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
